71aace0d234bb3fd3255bfdb508478e65a812bb4
NivaldoNeto <nivaldoveieiraneto@gmail.com>
2023-07-21 17:51:45 -0300

    adicionando titulos aos arquivos

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,8 +4,10 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +15,16 @@ class TarefasExport implements FromCollection
     public function collection()
     {
         return auth()->user()->tarefas()->get();
+    }
+
+    public function headings():array { //:array declarando o tipo de retorno
+        return [
+            'Id da Tarefa',
+            'Id do Usuário',
+            'Tarefa',
+            'Data Limite Conclusão',
+            'Data Criação', 
+            'Data Atualização'
+        ];
     }
 }
